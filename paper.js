1220be59446039085adc5ac68534f436d08af8fb
jamshedpur12 <76275831+jamshedpur12@users.noreply.github.com>
2021-03-06 12:01:14 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -4,13 +4,13 @@ class paper{
             isStatic:false,
             restitution:0.3,
             friction:0,
-            density:1.2,
+            density:1.2
         }
         this.x = x;
         this.y = y;
         this.r = r;
         this.image = loadImage("paper.png");
-        this.body = Bodies.circle(this.x,this.y,(this.r-20)/20,options);
+        this.body = Bodies.circle(this.x,this.y,(this.r-20)/2,options);
         World.add(world,this.body)
     }
     display(){
